2917fbab7b745542b3252f6b1fdbd07f1efa2113
Kennedy Soko <bsc-inf-16-21@unima.ac.mw>
2026-01-12 09:39:35 +0200

    creating student modules

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        fullName: 'Kennedy Soko',
+        registrationNumber: 'UNIMA/2022/001',
+        programme: 'Information Systems',
+        year: 4,
+      },
+      {
+        id: 2,
+        fullName: 'Lusper Mhango',
+        registrationNumber: 'UNIMA/2022/002',
+        programme: 'Computer Science',
+        year: 4,
+      },
+    ];
+  }
+}
