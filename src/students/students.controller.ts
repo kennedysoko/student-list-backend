efa5f0ff24f5174cac484530f8cabf0568380f23
Kennedy Soko <bsc-inf-16-21@unima.ac.mw>
2026-01-12 10:18:41 +0200

    testing real endpoints

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,24 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { StudentsService } from './students.service';
 
 @Controller('students')
 export class StudentsController {
+  constructor(private readonly studentsService: StudentsService) { }
+
   @Get()
   findAll() {
-    return [
-      {
-        id: 1,
-        fullName: 'Kennedy Soko',
-        registrationNumber: 'UNIMA/2022/001',
-        programme: 'Information Systems',
-        year: 4,
-      },
-      {
-        id: 2,
-        fullName: 'Lusper Mhango',
-        registrationNumber: 'UNIMA/2022/002',
-        programme: 'Computer Science',
-        year: 4,
-      },
-    ];
+    return this.studentsService.findAll();
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentsService {}
+export class StudentsService {
+    findAll() {
+        return [
+            {
+                id: 1,
+                name: 'John Doe',
+                course: 'Computer Science',
+                year: 2,
+            },
+        ];
+    }
+}
